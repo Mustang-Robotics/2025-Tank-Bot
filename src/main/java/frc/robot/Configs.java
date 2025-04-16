ddc6891f4714296a4b32d6703a6c5f34bb3d93c6
Mustang-Robotics <133044972+Mustang-Robotics@users.noreply.github.com>
2025-04-16 10:19:21 -0500

    Update Configs.java

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 
 public final class Configs {
     public static final class DriveSystem {
@@ -11,30 +12,31 @@ public final class Configs {
         public static final SparkMaxConfig rightFollowConfig = new SparkMaxConfig();
 
         static {
+
             leftLeadConfig
                 .idleMode(IdleMode.kBrake)
                 .smartCurrentLimit(30)
                 .voltageCompensation(12);
             
-            leftFollowConfig 
+            leftLeadConfig 
                 .encoder
                 .positionConversionFactor(2*Math.PI / 8.46)
                 .velocityConversionFactor(2*Math.PI / 60 / 8.46)
                 .uvwMeasurementPeriod(10)
                 .uvwAverageDepth(2);
 
-                leftLeadConfig
+            leftLeadConfig
+                .closedLoop
+                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                .pid(0.04, 0, 0)
+                .velocityFF(0)
+                .outputRange(-1, 1);
+
+            leftFollowConfig
                 .idleMode(IdleMode.kBrake)
                 .smartCurrentLimit(30)
                 .voltageCompensation(12)
                 .follow(31);
-            
-            leftFollowConfig 
-                .encoder
-                .positionConversionFactor(2*Math.PI / 8.46)
-                .velocityConversionFactor(2*Math.PI / 60 / 8.46)
-                .uvwMeasurementPeriod(10)
-                .uvwAverageDepth(2);
 
             rightLeadConfig
                 .idleMode(IdleMode.kBrake)
@@ -50,6 +52,13 @@ public final class Configs {
                 .uvwAverageDepth(2)
                 .inverted(true);
 
+            rightLeadConfig
+                .closedLoop
+                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                .pid(0.04, 0, 0)
+                .velocityFF(0)
+                .outputRange(-1, 1);
+
             rightFollowConfig
                 .idleMode(IdleMode.kBrake)
                 .smartCurrentLimit(30)
@@ -57,13 +66,6 @@ public final class Configs {
                 .inverted(true)
                 .follow(33);
             
-            rightFollowConfig 
-                .encoder
-                .positionConversionFactor(2*Math.PI / 8.46)
-                .velocityConversionFactor(2*Math.PI / 60 / 8.46)
-                .uvwMeasurementPeriod(10)
-                .uvwAverageDepth(2)
-                .inverted(true);
         }
     }
 }

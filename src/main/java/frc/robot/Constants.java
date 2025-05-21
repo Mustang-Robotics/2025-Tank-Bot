b0a5b93dce77a4387c0e709961f812e5cd1891da
Mustang-Robotics <robotics4174@blh.k12.mn.us>
2025-05-21 15:20:47 -0500

    Halfway through adding Constant Comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,6 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+
+//This section is all of the classes that are being use in this section of the code.
 package frc.robot;
 
 import com.pathplanner.lib.config.RobotConfig;
@@ -17,41 +27,37 @@ import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
+
+  //constants related to the driver(s) and controllers
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-    public static final double kDeadband = 0.1;
+    public static final int kDriverControllerPort = 0; //USB port of controller. Used in RobotContainer
+    public static final double kDeadband = 0.1; //Amount of joystick movement it takes to start registering. Used in ArcadeDrive command
   }
 
+  //constants related to the drivesystem
   public static final class DriveConstants {
 
-    public static final int kLeftLeader = 31;
-    public static final int kLeftFollower = 32;
-    public static final int kRightLeader = 33;
-    public static final int kRightFollower = 34;
-    public static final int kStallCurrent = 30;
-    public static final double kP = 0.0465;
-    public static final double kI = 0;
-    public static final double kD = 0;
-    public static final double kTrackwidth = 0.557;
-    public static final double kWheelRadiusMeters = Units.inchesToMeters(3);
+    public static final int kLeftLeader = 31; //CAN ID of LeftLeader Motor. Used in DriveSubsystem
+    public static final int kLeftFollower = 32; //CAN ID of LeftFollower Motor. Used in DriveSubsystem
+    public static final int kRightLeader = 33; //CAN ID of RightLeader Motor. Used in DriveSubsystem
+    public static final int kRightFollower = 34; //CAN ID of RightFollower Motor. Used in DriveSubsystem
+    public static final int kStallCurrent = 30; //Maximum amount of current that will get sent to a motor. Used in Configs
+    //The following numbers get used in calculations to help tune the speed of the drivetrain
+    public static final double kP = 0.0465; //Proportional Gain See Constants for explanation
+    public static final double kI = 0; //Integral Gain See Constants for explanation
+    public static final double kD = 0; //Derivative Gain See Constants for explanation
+    public static final double kTrackwidth = 0.557; //width from center to center of wheels measured in meters.
+    public static final double kWheelRadiusMeters = Units.inchesToMeters(3); //other way to use meters but not have to convert from inches
     public static final double kWheelCircumferenceMeters = kWheelRadiusMeters * 2 * Math.PI;
-    public static final double kNeoFreeSpeed = 5676;
-    public static final double kNeoKv = kNeoFreeSpeed / 12;
+    public static final double kNeoFreeSpeed = 5676; //RPMS of NEO motor. Can be looked up on REVRobotics
+    public static final double kNeoKv = kNeoFreeSpeed / 12; //12 is for 12V of battery
     public static final double kDrivingMotorFreeSpeedRps = kNeoFreeSpeed / 60;
-    public static final double kDrivingMotorReduction = 8.46;
+    public static final double kDrivingMotorReduction = 8.46; //Gear Ratio of gear box
     public static final double kDriveWheelFreeSpeed = 
-      (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters) / kDrivingMotorReduction;
-    public static final double kMaxSpeedMetersPerSec = 3.2;
-    public static final double kVoltPerSpeed = kDrivingMotorReduction * 60 / (kNeoKv * 2 * Math.PI * kWheelRadiusMeters);
+      (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters) / kDrivingMotorReduction; //calculates max speed of the wheel
+    public static final double kMaxSpeedMetersPerSec = 3.2; //limiting the max speed of the robot, this value is specific to MinneTrials
+    public static final double kVoltPerSpeed = kDrivingMotorReduction * 60 / (kNeoKv * 2 * Math.PI * kWheelRadiusMeters); //calculates how many volts it takes to move a specific speed
 
     
   }

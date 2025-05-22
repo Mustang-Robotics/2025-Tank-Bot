c95f3a0b47bd91a278aff6e02bc877b20bc86417
Mustang-Robotics <robotics4174@blh.k12.mn.us>
2025-05-22 11:54:58 -0500

    More Comments, and some name changes to make things more clear

---
+++ src/main/deploy/fieldlayout/FieldLayoutReadMe.txt
@@ -0,0 +1,4 @@
+Create a .json file of the april tags
+Use the 2024-minne-trials.json above for guidance
+Get the measurements from the field CAD
+For help with quaternion rotation - see team resorces
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,26 +1,4 @@
-/*
- * MIT License
- *
- * Copyright (c) PhotonVision
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+//Should only need to change the field name below to load the correct field
 
  package frc.robot.subsystems;
 
@@ -61,8 +39,11 @@ import java.util.List;
          tagCamera = new PhotonCamera(kTagCamera);
          //topCamera = new PhotonCamera(kTopCamera);
 
+         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         //Change name of field in child: to load the correct field/////////////////////////////////////////////////////
+         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
          try {
-            kTagLayout = new AprilTagFieldLayout(new File(Filesystem.getDeployDirectory(), "fieldlayout/2025-minne-trials.json").toString());
+            kTagLayout = new AprilTagFieldLayout(new File(Filesystem.getDeployDirectory(), "fieldlayout/2024-minne-trials.json").toString()); 
          } catch (Exception e) {
             e.printStackTrace();
          }
